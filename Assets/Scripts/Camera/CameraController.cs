ba279ded024b53fd21963be50506b6af3beb610d
kanestapler <kanestapler@gmail.com>
2016-05-24 16:15:27 -0500

    fixing camera to follow player at proper view

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float smoothing;
+	public Transform follow;
+
+	private Vector3 initialOffset;
+
+	void Start () {
+		initialOffset = transform.position - follow.position;
+	}
+
+	void FixedUpdate() {
+		Vector3 newPosition = initialOffset + follow.position;
+		transform.position = Vector3.Lerp (transform.position, newPosition, smoothing);
+	}
+
+}
