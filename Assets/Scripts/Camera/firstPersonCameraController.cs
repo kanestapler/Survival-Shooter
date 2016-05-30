69ae82ce3c9bdf432f34e63391966fbe38a481e3
kanestapler <kanestapler@gmail.com>
2016-05-30 01:41:05 -0500

    playing with first person camera

---
+++ Assets/Scripts/Camera/firstPersonCameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firstPersonCameraController : MonoBehaviour {
+
+	public Transform targetPosition;
+	public float smoothing = 3f;
+
+	void Awake () {
+	
+	}
+
+	void Update () {
+		transform.rotation = targetPosition.rotation;
+		transform.position = Vector3.Lerp (transform.position, targetPosition.position, smoothing);
+	}
+}
